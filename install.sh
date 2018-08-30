1d54bc593a5c8299e3d341007968978bb5ca5b16
Eduardo Koller <eduardotkoller@gmail.com>
2018-08-30 15:03:23 -0300

    oh-my-zsh custom folder only

--- install.sh
+++ install.sh
@@ -9,14 +9,15 @@ sudo apt update
 
 echo 'Installing all tools used...'
 sudo apt install fonts-font-awesome lxappearance rofi thunar zsh -y
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 echo 'Creating symbolic links to files in this folder'
 sudo ln -nfs $_curr/.config/i3/config $HOME/.config/i3/config
 sudo ln -nfs $_curr/.config/i3status/config $HOME/.config/i3status/config
 sudo ln -nfs $_curr/.fonts $HOME/.fonts
-sudo ln -nfs $_curr/.themes $HOME/.themes
+sudo ln -nfs $_curr/.themes/ $HOME/.themes/
 sudo ln -nfs $_curr/.zshrc $HOME/.zshrc
-sudo ln -nfs $_curr/.oh-my-zsh $HOME/.oh-my-zsh
+sudo ln -nfs $_curr/.oh-my-zsh/custom $HOME/.oh-my-zsh/custom
 sudo ln -nfs $_curr/cpanel /usr/bin/cpanel
 sudo chmod +x $_curr/cpanel
 
